2ce7f90a4cbfc8930404b152f7fe7b4195ed0853
S1monShu <segor4066@gmail.com>
2024-06-10 16:52:57 +0400

    third commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,21 +7,21 @@
     <title>{{ env('APP_NAME') }}</title>
     {{-- ICON     --}}
     <link rel="icon" href="{{ asset('/favicon.ico') }}">
-    <link rel="stylesheet" href="{{ asset('../../node_modules/vue-ssr-carousel/index.css')  }}">
+    <link rel="stylesheet" href="{{ asset('../../node_modules/vue-ssr-carousel/index.css') }}">
     <script src="https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit" async defer></script>
-    @vite('resources/css/app.css')
-    @vite('resources/css/icon-fonts.css')
-    @vite('resources/css/fonts.css')
-    @vite('resources/css/scrollbar.css')
-    @vite('resources/css/notifications.css')
-    @vite('resources/css/range-input.css')
+    @vite(['resources/css/app.css'])
+    @vite(['resources/css/icon-fonts.css'])
+    @vite(['resources/css/fonts.css'])
+    @vite(['resources/css/scrollbar.css'])
+    @vite(['resources/css/notifications.css'])
+    @vite(['resources/css/range-input.css'])
 </head>
 
 <body>
     <div id="app"></div>
-    @vite('resources/js/app.js')
-    @vite('resources/js/scripts/notifications.js')
-    @vite('resources/js/scripts/range-input.js')
+    @vite(['resources/js/app.js'])
+    @vite(['resources/js/scripts/notifications.js'])
+    @vite(['resources/js/scripts/range-input.js'])
 </body>
 
 </html>
